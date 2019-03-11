fe01dec0e22df81c08993d8aa6844c30e6ec31e7
anghel7 <ghehahanghel@gmail.com>
2019-03-11 17:35:21 -0400

    manejando rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClienteListComponent } from './cliente/cliente-list/cliente-list.component';
+import { ClienteCreateComponent } from './cliente/cliente-create/cliente-create.component';
+import { ClienteDetailsComponent } from './cliente/cliente-details/cliente-details.component';
+import { HomeComponent } from './layout/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'cliente/lista',
+    component: ClienteListComponent
+  },
+  {
+    path: 'cliente/crear',
+    component: ClienteCreateComponent
+  },
+  {
+    path: 'cliente/details',
+    component: ClienteDetailsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/cliente/cliente.module.ts
+++ src/app/cliente/cliente.module.ts
@@ -6,9 +6,10 @@ import { ClienteRoutingModule } from './cliente-routing.module';
 import { ClienteListComponent } from './cliente-list/cliente-list.component';
 import { ClienteCreateComponent } from './cliente-create/cliente-create.component';
 import { ClienteItemComponent } from './cliente-item/cliente-item.component';
+import { ClienteDetailsComponent } from './cliente-details/cliente-details.component';
 
 @NgModule({
-  declarations: [ClienteListComponent, ClienteCreateComponent, ClienteItemComponent],
+  declarations: [ClienteListComponent, ClienteCreateComponent, ClienteItemComponent, ClienteDetailsComponent],
   imports: [
     CommonModule,
     ClienteRoutingModule,
